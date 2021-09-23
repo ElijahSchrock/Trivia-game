4a9466bcb54ba763427ff4c0e176f7bc98076f45
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2021-09-22 19:39:00 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating a Trivia Quiz
+
+This is an activity for FSWD Lesson 1.10.7- Review.
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ index.js
@@ -0,0 +1,48 @@
+let userName = window.prompt("Please enter your name:");
+let userScore = 0
+
+for (let i = 0; i < questions.length; i++){
+    let question = questions[i];
+    let userAnswer = window.prompt(question.text);
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10;
+    }
+}
+
+ window.alert("Your score is: " + userScore);
+
+let serializedGames = localStorage.getItem("games");
+
+let games;
+if(serializedGames === null ){
+    games = []
+} else {
+    games = JSON.parse(serializedGames);
+}
+
+ let game = {
+     user: userName,
+     score: userScore
+ }
+ 
+ games.push(game);
+
+serializedGames = JSON.stringify(games);
+ localStorage.setItem( "games", serializedGames);
+
+let serializedHighScore = localStorage.getItem("scores");
+
+let scores;
+if(serializedHighScore === null){
+    scores = []
+} else {
+    scores = JSON.parse(serializedHighScore);
+}
+
+scores.push(game.score);
+
+serializedHighScore = JSON.stringify(game.score);
+ localStorage.setItem("scores", serializedHighScore);
+
+
+window.alert("The highscore is: " + serializedHighScore)
